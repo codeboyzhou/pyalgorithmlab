4a278150d80854076207d791712c219917da8239
codeboyzhou <imzhouchen@gmail.com>
2025-08-02 22:47:29 +0800

    docs(plotly3d): 为compute_plotly_camera_eye函数添加文档字符串

--- src/pyalgorithmlab/util/plotly3d.py
+++ src/pyalgorithmlab/util/plotly3d.py
@@ -2,6 +2,17 @@ import math
 
 
 def compute_plotly_camera_eye(elev: int = 0, azim: int = 0, camera_distance: float = 2.5) -> dict:
+    """
+    计算Plotly库的相机视角坐标
+
+    Args:
+        elev: 相机视角的仰角（单位：度）
+        azim: 相机视角的方位角（单位：度）
+        camera_distance: 相机距离
+
+    Returns:
+        相机视角坐标字典
+    """
     azim_rad = math.radians(azim)
     elev_rad = math.radians(elev)
     x = math.cos(azim_rad) * math.cos(elev_rad) * camera_distance
